b3e34876d9414a25186cb08277105ac0a54e7719
soubj <sobujahmed124@gmail.com>
2023-12-16 21:34:38 +0600

    styled chat icons

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,5 +1,21 @@
+import addFriend from "../../assets/icons/addFriend.png";
+import videoCall from "../../assets/icons/videoCall.png";
+import more from "../../assets/icons/more.png";
+import "./Chat.scss";
+
 const Chat = () => {
-  return <div className="chat">chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>john</span>
+        <div className="chatIcons">
+          <img src={videoCall} alt="" />
+          <img src={addFriend} alt="" />
+          <img src={more} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
